662708a19dadfd2d9f6c8515a7e45f614980f7dc
Amr <amrokasha76@gmail.com>
2026-04-25 13:26:12 +0300

    added more kaizo levels

--- kaizo1.lua
+++ kaizo1.lua
@@ -1,10 +1,10 @@
 local level = {
-    identifier = "BatTown",  -- change to a unique identifier
+    identifier = "kaizo_test",  -- change to a unique identifier
     title = "Bat Town",         -- level title
-    theme = THEME.VOLCANA,      -- must match selected level's theme
-    width = 4,                  -- must match level width
-    height = 4,                 -- must match level height
-    file_name = "Bat-Town.lvl", -- must match .lvl filename
+    theme = THEME.TIDE_POOL,      -- must match selected level's theme
+    width = 3,                  -- must match level width
+    height = 3,                 -- must match level height
+    file_name = "kaizo_.lvl", -- must match .lvl filename
 }
 
 local level_state = {loaded = false, callbacks = {}}
@@ -13,12 +13,48 @@ level.load_level = function()
     if level_state.loaded then return end
     level_state.loaded = true
 
+    level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function(entity, spawn_flags)
+		entity:destroy()
+	end, SPAWN_TYPE.SYSTEMIC, 0, ENT_TYPE.ITEM_PICKUP_SKELETON_KEY)
+	
+	level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function(entity, spawn_flags)
+		entity:destroy()
+	end, SPAWN_TYPE.ANY, 0, ENT_TYPE.MONS_SKELETON)
+
+	level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function(entity, spawn_flags)
+		entity:destroy()
+	end, SPAWN_TYPE.ANY, 0, ENT_TYPE.ITEM_SKULL)
+	
+	level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function(entity, spawn_flags)
+		entity:destroy()
+	end, SPAWN_TYPE.ANY, 0, ENT_TYPE.ITEM_BONES)
+
+    level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function(entity)
+		entity:set_cursed(true)
+	end, SPAWN_TYPE.ANY, 0, ENT_TYPE.MONS_MANTRAP)
+
+    level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function (entity)
+		entity.flags = set_flag(entity.flags, 6)
+    end, SPAWN_TYPE.ANY, 0, ENT_TYPE.FLOOR_GENERIC)
+
+	level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function (entity)
+		entity.flags = set_flag(entity.flags, 6)
+    end, SPAWN_TYPE.ANY, 0, ENT_TYPE.FLOORSTYLED_PAGODA)
+
+	level_state.callbacks[#level_state.callbacks+1] = set_post_entity_spawn(function (entity)
+        -- Remove Hermitcrabs
+        local x, y, layer = get_position(entity.uid)
+        local floor = get_entities_at(0, MASK.ANY, x, y, layer, 1)
+        if #floor > 0 then
+            entity.flags = set_flag(entity.flags, ENT_FLAG.INVISIBLE)
+            entity:destroy()
+        end
+    end, SPAWN_TYPE.ANY, 0, ENT_TYPE.MONS_HERMITCRAB)
+
     level_state.callbacks[#level_state.callbacks + 1] = set_callback(function()
-        
         if players[1] and players[1].inventory.kills_level > 0 then
             players[1].health = 0
         end
-
     end, ON.GAMEFRAME)
 
     toast(level.title)
